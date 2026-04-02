2e96f0d61cd7ff2282e1b0a74362db399c00df02
Ferreirabento <jf6808932@gmai.com>
2026-04-02 14:33:15 -0300

    Aqui eu descobri que o maior consumo de alcool feminino na china foi nas datas 6/2/2020, 8/5/2020 e 5/25/2020 talvez tenha um padrão aqui...

--- alcool.session.sql
+++ alcool.session.sql
@@ -17,3 +17,12 @@ FROM tabela_principal
 WHERE Gender = "Female"
 GROUP BY Countries
 ORDER BY Count(Gender) DESC;
+
+
+-- descobri que as mulheres na chinna foram as que mais consumiram alcool no mundo inteiro --
+-- agora preciso saber em que periodo isso ocorre então a minha população vai ser as mulhres chinesas --
+SELECT DISTINCT(Date), COUNT(Gender)
+FROM tabela_principal
+WHERE Gender = "Female" and Countries = "China"
+GROUP BY Date
+ORDER BY  COUNT(Gender) DESC;
