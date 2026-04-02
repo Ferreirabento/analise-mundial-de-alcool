ef7a9f8fc6a5a5727d55f57afd85686ff8d50cd1
Ferreirabento <jf6808932@gmai.com>
2026-04-01 21:05:24 -0300

    aqui eu resolvi escoler uma população para os meus testes estatisticos com sql para preparar os dados e por fim analisalos

--- alcool.session.sql
+++ alcool.session.sql
@@ -1,4 +1,11 @@
 use alcool;
 
-SELECT * 
+SELECT Countries
 FROM tabela_principal;
+
+-- escolhendo a população para usar estatistica --
+-- resolvi escolher um pais aleatorio sem motivo aparente -- 
+SELECT Gender, Countries
+FROM tabela_principal
+WHERE Gender = "Male" and Countries = "Brazil"
+ORDER BY Countries;
